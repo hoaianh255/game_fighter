db84c8f11fcbbcda4869c1df280de3cd45174700
nguyen hoai anh <hoaianh0123450@gmail.com>
2022-04-23 20:40:51 +0700

    complete Game timer and game over function

--- script.js
+++ script.js
@@ -33,7 +33,31 @@ const keys = {
     pressed: false
   }
 }
+let timer = 15;
+let timeoutID;
+function determineWiner({player,enemy,timeoutID}){
+  clearTimeout(timeoutID)
+  if(player.health == enemy.health){
+    document.querySelector('#displayText').innerHTML = 'tie'
+  }else if(player.health > enemy.health){
+    document.querySelector('#displayText').innerHTML = 'player 1 wins'
+  }else if(player.health < enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'player 2 wins'
+  }
+  
+}
+function decreaseTimer() {
+  timeoutID = setTimeout(decreaseTimer, 1000);
+  if (timer > 0) {
+    timer--;
+    document.querySelector('#timer').innerHTML = timer;
+  }
+  if(timer == 0) {
+    determineWiner({player,enemy,timeoutID})
+  }
+}
 
+decreaseTimer();
 // class
 class Sprite {
   constructor({ position, velocity, color, offset }) {
@@ -227,6 +251,10 @@ function animate() {
     enemy.isAttacking = false;
     player.health -= 20;
     document.querySelector('#playerhealth').style.width = player.health + '%';
+  }
+  // end game based on health
+  if(player.health <= 0 || enemy.health <=0 ){
+    determineWiner({player,enemy,timeoutID});
   }
 }
 animate()
